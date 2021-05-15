0f0bc4a744105dd41f706baf42eee80ff2ed9cb5
Buğra Coşkun <45716038+bugracoskun@users.noreply.github.com>
2021-05-15 16:46:17 +0300

    v3

---
+++ src/data/sample5/cloud.js
@@ -0,0 +1,67 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 18922301,
+    "boundingBox": {
+        "lx": -47.36032424316406,
+        "ly": -125.72429567871095,
+        "lz": -1.8207119384765634,
+        "ux": 136.83167575683596,
+        "uy": 58.46770432128906,
+        "uz": 182.37128806152345
+    },
+    "tightBoundingBox": {
+        "lx": -47.36032424316406,
+        "ly": -125.72429567871095,
+        "lz": -1.8207119384765634,
+        "ux": 81.47767575683594,
+        "uy": 58.46770432128906,
+        "uz": 54.515288061523438
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 1.5951495170593262,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
